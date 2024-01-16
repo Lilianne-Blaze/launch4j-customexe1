83dbc665d2b3c89e4ff04179defaa686674578b7
Lilianne-Blaze <39486911+Lilianne-Blaze@users.noreply.github.com>
2024-01-16 15:34:16 +0100

    Added simple demo 1

---
+++ launch4j-customexe1-demo1/src/main/java/com/example/launch4j_customexe1_demo1/Launch4jCustomexe1Demo1.java
@@ -0,0 +1,11 @@
+package com.example.launch4j_customexe1_demo1;
+
+public class Launch4jCustomexe1Demo1 {
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello World!");
+        System.out.println("Press ctrl+c to continue");
+	Thread.sleep(60000);
+	
+    }
+}
